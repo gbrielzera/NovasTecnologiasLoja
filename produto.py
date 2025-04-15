090fecde0ceebdcdff90a15c5624efce4c122d37
Gabriel Cézar <151453514+gbrielzera@users.noreply.github.com>
2025-04-15 15:52:56 -0300

    Update produto.py

--- produto.py
+++ produto.py
@@ -4,8 +4,5 @@ class Produto:
         self.preco = preco
         self.estoque = estoque
 
-    def usarDesconto(self, porcento):
-        self.preco -= self.preco * (porcento / 100)
-
     def __str__(self):
         return f"{self.nome} - R${self.preco:.2f} - Estoque: {self.estoque}"
